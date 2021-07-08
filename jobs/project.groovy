2ae15b73b22a1eaaab7314849affceb32bbc17aa
Rajesh Jonnalagadda <vlnrajeshj@gmail.com>
2021-07-08 20:27:32 +0530

    Adding Folders

--- jobs/project.groovy
+++ jobs/project.groovy
@@ -1,12 +1,17 @@
 def owner = 'vlnrajesh'
 def project = 'jenkins-seeds'
 def branchName = 'main'
-def jobName = "${project}-${branchName}".replaceAll('/','-')
+def scmRepo='github.com'
+def jobName = "${scmRepo}${project}-${branchName}".replaceAll('/','-')
+  folder(scmRepo) {
+    displayName(titleCase(scmrepo))
+    description('Projects created from titleCase(scmRepo))
+  }
   job(jobName) {
     scm {
         git {
             remote {
-              github("${owner}/${project}")
+              url("git@${scmRepo}:${owner}/${project}")
             }
             branch("${branchName}")
         }
